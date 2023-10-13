79d642672bd004c8486bf38df17ab36c011b35d9
zalditri10 <zalditrighifari@gmail.com>
2023-10-13 09:02:27 +0700

    second

---
+++ application/views/view-contoh1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Contoh 1</title>
+    </head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar Web Programming..!!! <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1. " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
